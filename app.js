9a24a49189b8586a8b684699fec1d834ac34974d
Parul Rawat <parul.rawat@comprotechnologies.com>
2019-04-03 16:16:15 +0530

    Creating Folder Path

--- app.js
+++ app.js
@@ -2,32 +2,26 @@ const fs=require('fs');
 const util=require('util')
 var path=require('path');
 var process=require('process');
-
+var configPath;
 var utility=require('./utility.js')
 
-var arr=new Array();
+//var arr=new Array();
 utility.fetchJSONdata('./init.json')
     .then((result) => {
         // enable status true  //and if the directory mentioned exists
         if (JSON.parse(result).enable === 'true' && fs.statSync(JSON.parse(result).path).isDirectory() === true) {
             //config path
-            arr.push(JSON.parse(result).path);
-
+            configPath=JSON.parse(result).path;
+            return configPath;
         }
-        // else {console.log("set enable to true");}
     })
-    .catch((error) => {
+    .then((configPath)=>{
+        //console.log("2");
+        var taskFolderPath=utility.createFolderPath(configPath);
+        //createFolder(taskFolderPath);
+        //console.log(taskFolderPath);
+    })
+    .catch((error)=>{
         console.log(error);
-    });
-    console.log(configPath);
-utility.inputTaskID()
-    .then((taskID) => {
-        // utility.createFolderPath(taskID);
-        console.log(taskID);
-        console.log(configPath);
     })
-    .catch(() => {
-        console.log("Please enter valid Task ID");
-});
-
 
--- utility.js
+++ utility.js
@@ -1,10 +1,13 @@
 const fs=require('fs');
 const util=require('util');
 var process=require('process');
+const makeDir = require('make-dir');
+const path=require('path');
+var mkdirp = require('mkdirp');
 
 //JSON path
 var data={
-        fetchJSONdata:function(file) {
+    fetchJSONdata : function (file) {
             return new Promise((resolve, reject) => {
                 fs.readFile(file, (error,data) => {
                     if (error) reject(error);
@@ -12,21 +15,46 @@ var data={
                 });
         });
     },
-    inputTaskID:function() {
-        return new Promise((resolve,reject)=>{
+    inputTaskID : function () {    
             var taskID=process.argv[2];
-            (!taskID)? reject() : resolve (taskID);
-            // if (!taskID) {
-            //     reject (error);
-            // }
-            // else {
-            //     resolve (taskID);
-            // }
-        });
-        
+            //console.log(taskID)
+            if (!taskID) {
+                throw new Error('Please enter Task ID');
+            }
+            else console.log('task ID taken');
+            return taskID;   
     },
-    createFolderPath(taskID,configPath) {
-
+    createFolderPath : function (configPath) {
+        //1.fetch task ID
+        var taskID=this.inputTaskID();
+        //2. split the string in array
+        var splitTaskID= this.splitTaskIDString(taskID); //3. now append the folder path
+        var folderPath=`${configPath}\\XMLs\\TaskXmls2019\\${splitTaskID[0]}\\${splitTaskID[1]}\\${splitTaskID[2]}`;
+        debugger;
+       if(fs.statSync(folderPath).isDirectory() === true){          //if directory exists
+        //    console.log(path.parse(folderPath));
+            console.log(process.config);
+            console.log(process.env);
+            return folderPath;
+       }
+       else{ 
+           mkdirp(folderPath,(err)=>{
+        if(err) {console.log(err)}
+        else {console.log("sucvces")
+         return folderPath;}
+    });} // make directory to be done later : fs.mkdirSync(folderPath);
+    },
+    splitTaskIDString : function (taskID) {
+        var arr=new Array();        
+        arr=taskID.split(".");      
+        arr[0]=arr[0].replace(/\d/gi,'')
+        return arr;
+    },
+    createFolder : function (taskFolderPath) {
+        mkdirp('taskFolderPath',(err)=>{
+            if(err) {console.log(err)}
+            else {console.log("sucvces")}
+        });
     }
 }
 module.exports=data;

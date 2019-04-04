f7ba447543d80acb831811f85b5c2bccb88463c0
Parul Rawat <parul.rawat@comprotechnologies.com>
2019-04-04 18:35:24 +0530

    fetching Rebase tool

--- app.js
+++ app.js
@@ -3,23 +3,35 @@ const util=require('util')
 var path=require('path');
 var process=require('process');
 var configPath;
+var stepConcatenationPath;
 var utility=require('./utility.js')
+// var {taskID}=require('./utility.js')
 
 //var arr=new Array();
 utility.fetchJSONdata('./init.json')
     .then((result) => {
         // enable status true  //and if the directory mentioned exists
-        if (JSON.parse(result).enable === 'true' && fs.statSync(JSON.parse(result).path).isDirectory() === true) {
+        if (JSON.parse(result).enable === 'true'
+             && fs.statSync(JSON.parse(result).path).isDirectory() === true
+            && fs.statSync(JSON.parse(result).stepConcatenation).isDirectory() === true) {
             //config path
             configPath=JSON.parse(result).path;
+            stepConcatenationPath=JSON.parse(result).stepConcatenation;
             return configPath;
         }
     })
     .then((configPath)=>{
-        //console.log("2");
-        var taskFolderPath=utility.createFolderPath(configPath);
-        //createFolder(taskFolderPath);
-        //console.log(taskFolderPath);
+        // console.log(stepConcatenationPath);
+        // console.log(this.taskID);
+        var x=utility.createFolderPath(configPath);
+        // utility.fetchJSONdata.bind(x);
+        //console.log(stepConcatenationPath);
+         var data=utility.updateLocalInputJson(configPath,utility.taskID);
+         utility.overWriteInputJson (stepConcatenationPath,data)
+         utility.rebase(stepConcatenationPath)
+         
+
+
     })
     .catch((error)=>{
         console.log(error);
--- utility.js
+++ utility.js
@@ -4,9 +4,12 @@ var process=require('process');
 const makeDir = require('make-dir');
 const path=require('path');
 var mkdirp = require('mkdirp');
+var cp = require("child_process");
 
 //JSON path
 var data={
+    taskID:'',
+    folderName:'',
     fetchJSONdata : function (file) {
             return new Promise((resolve, reject) => {
                 fs.readFile(file, (error,data) => {
@@ -16,48 +19,52 @@ var data={
         });
     },
     inputTaskID : function () {    
-            var taskID=process.argv[2];
-            //console.log(taskID)
-            if (!taskID) {
+
+            this.taskID=process.argv[2];
+
+            if (!this.taskID) {
                 throw new Error('Please enter Task ID');
             }
-            else console.log('task ID taken');
-            return taskID;   
+            //else console.log('task ID taken'); +
+            return this.taskID;   
     },
     createFolderPath : function (configPath) {
         //1.fetch task ID
-        var taskID=this.inputTaskID();
+        this.taskID=this.inputTaskID();
+
         //2. split the string in array
-        var splitTaskID= this.splitTaskIDString(taskID); //3. now append the folder path
+        var splitTaskID= this.splitTaskIDString(this.taskID); //3. now append the folder path
+
         var folderPath=`${configPath}\\XMLs\\TaskXmls2019\\${splitTaskID[0]}\\${splitTaskID[1]}\\${splitTaskID[2]}`;
-        var folderName=`${folderPath}\\${splitTaskID[3]}.${splitTaskID[4]}.${splitTaskID[5]}`;
-        console.log(folderName);
-        this.createFolder(folderPath,folderName);
+        this.folderName=`${folderPath}\\${splitTaskID[3]}.${splitTaskID[4]}.${splitTaskID[5]}`;
+        //console.log(this.folderName); +
+        
+        //console.log(`Task to be created at ${this.folderName}`);+
+        this.createFolder(folderPath,this.folderName);
+        return this;
 },
     splitTaskIDString : function (taskID) {
         var arr=new Array();        
         arr=taskID.split(".");      
         arr[0]=arr[0].replace(/\d/gi,'').toLowerCase();
         arr[1]=arr[1].toLowerCase();
-        console.log(arr)
+        // console.log(arr)
         return arr;
     },
     createFolder : function (folderPath,folderName) {
-        console.log(fs.statSync(folderPath).isDirectory());
-        if (fs.statSync(folderPath).isDirectory() === true){    
-            console.log("in if")  //if directory exists
+        //console.log(fs.statSync(folderPath).isDirectory());+
+        if (fs.statSync(folderPath).isDirectory() === true){     //if directory exists
             fs.mkdir(folderName,(err)=>{
                 if(err) {
                     console.log(err);
                 }
                 else {
-                    console.log('success');
+                    console.log('Success in creating Task Folder');
                 }
             })
             return folderPath;
-       }
+        }
        else { 
-           console.log("in else");
            fs.mkdir(folderPath,(err)=>{
                 if(err) {console.log(err)}
                 else {console.log("success")}
@@ -66,7 +73,48 @@ var data={
                 if(err) {console.log(err)}
                 else {console.log("success")}
             });
-    }
+        }
+    },
+    updateLocalInputJson : function (configPath,taskID) {
+
+        taskID=taskID.replace(/19/,'16');
+       var xmls= this.folderName.split('sim5service\\');
+
+        var input={
+            steps:[{
+                id : `${taskID}`,
+                type: "id"
+            }],
+            destination : `${xmls[1]}`,
+            mode: "1"
+        };
+        let data = JSON.stringify(input,null,2);  
+        //console.log(data);
+        fs.writeFileSync('util.json', data);  
+        return data;
+    },
+    overWriteInputJson : function (stepConcatenationPath,data) {
+        fs.writeFile(`${stepConcatenationPath}\\input.json`, data, function (err) {
+            if (err) {
+                return console.log("Error writing file to input.json " + err);
+            }
+        });
+    },
+    rebase : function(stepConcatenationPath) {
+
+    cp.exec("node dist/index.js --input=input.json", {cwd: stepConcatenationPath}, function(error,stdout,stderr){
+        if (error) {
+            console.log("error while rebasing:"+error);
+        }
+        else if (stdout) {
+            console.log(stdout);
+        }
+        else if (stderr) {
+            console.log(stderr);
+        }
+        
+    });
+    console.log("finished");
     }
 }
 module.exports=data;
